5287f540d694b1cee36966591407ca8a97440c39
eoghan <eoghancurtin93@gmail.com>
2023-09-10 13:35:03 +0800

    test

---
+++ src/ecr.ts
@@ -0,0 +1,43 @@
+import {
+  ECRClient,
+  DescribeImageScanFindingsCommand,
+  ScanNotFoundException,
+} from "@aws-sdk/client-ecr";
+
+const client = new ECRClient({ region: "ap-southeast-2" });
+const command = new DescribeImageScanFindingsCommand({
+  repositoryName: "amazon-inspector-check-test",
+  imageId: {
+    imageTag: "latest",
+  },
+});
+
+function delay(milliseconds: number) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, milliseconds);
+  });
+}
+
+async function main() {
+  scan();
+}
+
+const scan = async () => {
+  while (true) {
+    try {
+      let x = await client.send(command);
+      console.log(x);
+      await delay(100);
+      break;
+    } catch (err: unknown) {
+      if (err instanceof ScanNotFoundException) {
+        console.log("Scan Incomplete waiting 100ms");
+        continue;
+      } else {
+        console.log("ERROR: ", err);
+      }
+    }
+  }
+};
+
+main();
