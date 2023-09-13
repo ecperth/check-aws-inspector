bb88cc61a31a0d4ca832d7aa31a93efcbd6caa7e
eoghan <eoghancurtin93@gmail.com>
2023-09-14 07:12:12 +0800

    tests dependencies

---
+++ __tests__/index.test.ts
@@ -0,0 +1,5 @@
+import * as index from "../src/index"
+
+it('sets the time output', async () => {
+    expect(2).toBeGreaterThan(1);
+});
--- src/index.ts
+++ src/index.ts
@@ -2,37 +2,40 @@ import * as core from '@actions/core';
 import { getImageScanFindings } from './ecr';
 import { findingSeverities, ScanFindings } from './scanner';
 
-const repository = core.getInput('repository', { required: true });
-const tag = core.getInput('tag', { required: true });
-const failOn = core.getInput('fail-on');
-const ignore = core.getInput('ignore');
-const maxRetries = core.getInput('max-retries', { required: true });
-const delay = core.getInput('delay', { required: true });
-const consistencyDelay = core.getInput('consistency-delay', { required: true });
+run();
+export async function run(){
+  const repository = core.getInput('repository', { required: true });
+  const tag = core.getInput('tag', { required: true });
+  const failOn = core.getInput('fail-on');
+  const ignore = core.getInput('ignore');
+  const maxRetries = core.getInput('max-retries', { required: true });
+  const delay = core.getInput('delay', { required: true });
+  const consistencyDelay = core.getInput('consistency-delay', { required: true });
+  
+  const ignoreList = '' ? [] : ignore.trim().replace(/\n|\s/g, ',').split(',');
 
-const ignoreList = '' ? [] : ignore.trim().replace(/\n|\s/g, ',').split(',');
-
-if (validateInput(failOn, maxRetries, delay, consistencyDelay)) {
-  getImageScanFindings(
-    repository,
-    tag,
-    failOn,
-    ignoreList,
-    +delay,
-    +maxRetries,
-    +consistencyDelay,
-  )
-    .then((scanFindings: ScanFindings) => {
-      core.setOutput(
-        'findingSeverityCounts',
-        scanFindings.findingSeverityCounts,
-      );
+  if (validateInput(failOn, maxRetries, delay, consistencyDelay)) {
+    try {
+      const scanFindings = await getImageScanFindings(
+        repository,
+        tag,
+        failOn,
+        ignoreList,
+        +delay,
+        +maxRetries,
+        +consistencyDelay,
+      )
+      core.setOutput('findingSeverityCounts', scanFindings.findingSeverityCounts);
       if (scanFindings.errorMessage) {
         core.setFailed(scanFindings.errorMessage);
       }
-    })
-    .catch((err) => core.setFailed(err.message));
-}
+      } catch (err){
+        if (err instanceof Error){
+          core.setFailed(err.message)
+        }
+      }
+    } 
+  }
 
 function validateInput(
   failOn: string,
